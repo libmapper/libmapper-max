6df792ee2515cf7533f2193b2f4f4dfc65986ae7
Joseph Malloch <joseph.malloch@gmail.com>
2023-09-18 21:06:25 -0300

    Updated macos build script

--- macos_build.sh
+++ macos_build.sh
@@ -1,3 +1,5 @@
+export MACOSX_DEPLOYMENT_TARGET=10.10
+
 echo cleaning up dist folder...
 rm -r ./dist
 
@@ -51,7 +53,7 @@ else
     fi
     cd ./liblo
     echo build liblo: arm
-    (./configure CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -fPIC" CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin" --build=x86_64-apple-darwin19.6.0 --host=aarch64-apple-darwin --prefix=$PWD/../inst --disable-tests --disable-tools --disable-examples || (cat config.log; false))
+    (./configure CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -fPIC -mmacosx-version-min=10.10" CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -mmacosx-version-min=10.10" LDFLAGS="-mmacosx-version-min=10.10" --build=x86_64-apple-darwin19.6.0 --host=aarch64-apple-darwin --prefix=$PWD/../inst --disable-tests --disable-tools --disable-examples || (cat config.log; false))
     make install
     cd src
     gcc -dynamiclib -Wl,-undefined -Wl,dynamic_lookup -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -o .libs/liblo.7.dylib  .libs/liblo_la-address.o .libs/liblo_la-send.o .libs/liblo_la-message.o .libs/liblo_la-server.o .libs/liblo_la-method.o .libs/liblo_la-blob.o .libs/liblo_la-bundle.o .libs/liblo_la-timetag.o .libs/liblo_la-pattern_match.o .libs/liblo_la-version.o .libs/liblo_la-server_thread.o -O0 -g -install_name  ../inst/lib/liblo.7.dylib -Wl,-single_module
@@ -59,14 +61,20 @@ else
     mv ./src/.libs/liblo.7.dylib ./../inst/lib/liblo.7.dylib.arm64
     echo build liblo: x86_64
     make clean
-    (./configure --host=$HOST --prefix=$PWD/../inst --disable-tests --disable-tools --disable-examples CFLAGS=-fPIC || (cat config.log; false))
+#    (./configure --host=$HOST --prefix=$PWD/../inst --disable-tests --disable-tools --disable-examples CFLAGS=-fPIC || (cat config.log; false))
+    (./configure CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=x86_64-apple-darwin -fPIC -mmacosx-version-min=10.10" CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=x86_64-apple-darwin -mmacosx-version-min=10.10" LDFLAGS="-mmacosx-version-min=10.10" --build=arm-apple-darwin --host=x86_64-apple-darwin --prefix=$PWD/../inst --disable-tests --disable-tools --disable-examples || (cat config.log; false))
     make install
+    cd src
+    gcc -dynamiclib -Wl,-undefined -Wl,dynamic_lookup -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=x86_64-apple-darwin -o .libs/liblo.7.dylib  .libs/liblo_la-address.o .libs/liblo_la-send.o .libs/liblo_la-message.o .libs/liblo_la-server.o .libs/liblo_la-method.o .libs/liblo_la-blob.o .libs/liblo_la-bundle.o .libs/liblo_la-timetag.o .libs/liblo_la-pattern_match.o .libs/liblo_la-version.o .libs/liblo_la-server_thread.o -O0 -g -install_name  ../inst/lib/liblo.7.dylib -Wl,-single_module
+    cd ..
     mv ./src/.libs/liblo.7.dylib ./../inst/lib/liblo.7.dylib.x86_64
     echo build liblo universal binary
     cd ./../inst/lib
     lipo -create -output ./liblo.7.dylib ./liblo.7.dylib.arm64 ./liblo.7.dylib.x86_64
     lipo -info ./liblo.7.dylib
     rm ./liblo.7.dylib.*
+    echo fixing paths
+    install_name_tool -id liblo.7.dylib liblo.7.dylib
     cd ../..
 fi
 
@@ -89,20 +97,24 @@ else
     fi
     cd libmapper
     echo building libmapper: arm
-    ./autogen.sh CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin" CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin" --build=x86_64-apple-darwin19.6.0 --host=aarch64-apple-darwin --prefix=$PWD/../inst PKG_CONFIG_PATH=$PWD/../inst/lib/pkgconfig
+    ./autogen.sh CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -mmacosx-version-min=10.10" CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -mmacosx-version-min=10.10" LDFLAGS="-mmacosx-version-min=10.10" --build=x86_64-apple-darwin19.6.0 --host=aarch64-apple-darwin --prefix=$PWD/../inst PKG_CONFIG_PATH=$PWD/../inst/lib/pkgconfig --disable-python --disable-java --disable-csharp --disable-audio --disable-docs --disable-tests
     make install
     echo trying to fix error
     cd ./src/.libs
-    clang -dynamiclib -Wl,-undefined -Wl,dynamic_lookup -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -o libmapper.11.dylib libmapper_la-device.o libmapper_la-expression.o libmapper_la-graph.o libmapper_la-link.o libmapper_la-list.o libmapper_la-map.o libmapper_la-message.o libmapper_la-network.o libmapper_la-object.o libmapper_la-path.o libmapper_la-property.o libmapper_la-signal.o libmapper_la-slot.o libmapper_la-table.o libmapper_la-time.o libmapper_la-mpr_set_coerced.o libmapper_la-value.o -L$PWD/../../../inst/lib $PWD/../../../inst/lib/liblo.dylib -lz -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -install_name $PWD/../../../inst/lib/libmapper.11.dylib -Wl,-single_module
+    gcc -dynamiclib -Wl,-undefined -Wl,dynamic_lookup -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=arm64-apple-darwin -o libmapper.11.dylib libmapper_la-device.o libmapper_la-expression.o libmapper_la-graph.o libmapper_la-link.o libmapper_la-list.o libmapper_la-map.o libmapper_la-message.o libmapper_la-network.o libmapper_la-object.o libmapper_la-path.o libmapper_la-property.o libmapper_la-signal.o libmapper_la-slot.o libmapper_la-table.o libmapper_la-time.o libmapper_la-mpr_set_coerced.o libmapper_la-value.o -L$PWD/../../../inst/lib $PWD/../../../inst/lib/liblo.dylib -lz -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -install_name $PWD/../../../inst/lib/libmapper.11.dylib -Wl,-single_module
     cd ../..
     mv ./src/.libs/libmapper.11.dylib ./../inst/lib/libmapper.11.dylib.arm64
     file ./../inst/lib/libmapper.11.dylib.arm64
 
     echo building libmapper: x86_64
     make clean
-    ./autogen.sh --prefix=$PWD/../inst PKG_CONFIG_PATH=$PWD/../inst/lib/pkgconfig --disable-python --disable-java --disable-csharp --disable-audio --disable-docs --disable-tests
+#    ./autogen.sh --prefix=$PWD/../inst PKG_CONFIG_PATH=$PWD/../inst/lib/pkgconfig --disable-python --disable-java --disable-csharp --disable-audio --disable-docs --disable-tests
+    ./configure CFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=x86_64-apple-darwin -mmacosx-version-min=10.10" CXXFLAGS="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=x86_64-apple-darwin -mmacosx-version-min=10.10" LDFLAGS="-mmacosx-version-min=10.10" --build=arm-apple-darwin --host=x86_64-apple-darwin --prefix=$PWD/../inst PKG_CONFIG_PATH=$PWD/../inst/lib/pkgconfig --disable-python --disable-java --disable-csharp --disable-audio --disable-docs --disable-tests
     make install
-    mv ./../inst/lib/libmapper.11.dylib ./../inst/lib/libmapper.11.dylib.x86_64
+    cd ./src/.libs
+    gcc -dynamiclib -Wl,-undefined -Wl,dynamic_lookup -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk --target=x86_64-apple-darwin -o libmapper.11.dylib libmapper_la-device.o libmapper_la-expression.o libmapper_la-graph.o libmapper_la-link.o libmapper_la-list.o libmapper_la-map.o libmapper_la-message.o libmapper_la-network.o libmapper_la-object.o libmapper_la-path.o libmapper_la-property.o libmapper_la-signal.o libmapper_la-slot.o libmapper_la-table.o libmapper_la-time.o libmapper_la-mpr_set_coerced.o libmapper_la-value.o -L$PWD/../../../inst/lib $PWD/../../../inst/lib/liblo.dylib -lz -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -install_name $PWD/../../../inst/lib/libmapper.11.dylib -Wl,-single_module
+    cd ../..
+    mv ./src/.libs/libmapper.11.dylib ./../inst/lib/libmapper.11.dylib.x86_64
     echo libmapper: building universal binary
     cd ./../inst/lib
     file ./libmapper.11.dylib.arm64
@@ -110,6 +122,9 @@ else
     lipo -create -output ./libmapper.11.dylib ./libmapper.11.dylib.arm64 ./libmapper.11.dylib.x86_64
     file ./libmapper.11.dylib
     rm ./libmapper.11.dylib.*
+    echo fixing paths
+    install_name_tool -id libmapper.11.dylib libmapper.11.dylib
+    install_name_tool -change ../inst/lib/liblo.7.dylib @loader_path/liblo.7.dylib libmapper.11.dylib
     cd ../..
 fi
 
@@ -182,15 +197,38 @@ cp ../mpr.out/mpr.out.maxhelp ../dist/Max/Mapper/help/
 mv oscmulticast.mxo ../dist/Max/Mapper/externals
 cp ../oscmulticast/oscmulticast.maxhelp ../dist/Max/Mapper/help/
 
-echo bundling dylibs
+echo fixing library paths
 cd ../
-./dylibbundler/dylibbundler -cd -p '@loader_path/../../../../support/' -x ./dist/Max/Mapper/externals/mapper.mxo/Contents/MacOS/mapper -d ./dist/Max/Mapper/support/
-./dylibbundler/dylibbundler -cd -p '@loader_path/../../../../support/' -x ./dist/Max/Mapper/externals/mpr.device.mxo/Contents/MacOS/mpr.device -d ./dist/Max/Mapper/support/
-./dylibbundler/dylibbundler -cd -p '@loader_path/../../../../support/' -x ./dist/Max/Mapper/externals/mpr.in.mxo/Contents/MacOS/mpr.in -d ./dist/Max/Mapper/support/
-./dylibbundler/dylibbundler -cd -p '@loader_path/../../../../support/' -x ./dist/Max/Mapper/externals/mpr.out.mxo/Contents/MacOS/mpr.out -d ./dist/Max/Mapper/support/
-./dylibbundler/dylibbundler -cd -p '@loader_path/../../../../support/' -x ./dist/Max/Mapper/externals/oscmulticast.mxo/Contents/MacOS/oscmulticast -d ./dist/Max/Mapper/support/
+echo PWD: $PWD
 
-./dylibbundler/dylibbundler -cd -p '@loader_path/libs/' -x ./dist/pd/mapper/mapper.pd_darwin -d ./dist/pd/mapper/libs/
+# store the previous libmapper path
+LIBLO_PATH=`otool -L dist/Max/Mapper/externals/mapper.mxo/Contents/MacOS/mapper | grep -m 1 '.*liblo.7.dylib' | sed -e 's/    //g' -e 's/ (comp.*//g'` && export LIBLO_PATH
+LIBMAPPER_PATH=`otool -L dist/Max/Mapper/externals/mapper.mxo/Contents/MacOS/mapper | grep -m 1 '.*libmapper.11.dylib' | sed -e 's/    //g' -e 's/ (comp.*//g'` && export LIBMAPPER_PATH
+
+echo processing mapper.mxo
+install_name_tool -change $LIBLO_PATH @loader_path/../../../../support/liblo.7.dylib dist/Max/Mapper/externals/mapper.mxo/Contents/MacOS/mapper
+install_name_tool -change $LIBMAPPER_PATH @loader_path/../../../../support/libmapper.11.dylib dist/Max/Mapper/externals/mapper.mxo/Contents/MacOS/mapper
+
+echo processing mpr.device.mxo
+install_name_tool -change $LIBLO_PATH @loader_path/../../../../support/liblo.7.dylib dist/Max/Mapper/externals/mpr.device.mxo/Contents/MacOS/mpr.device
+install_name_tool -change $LIBMAPPER_PATH @loader_path/../../../../support/libmapper.11.dylib dist/Max/Mapper/externals/mpr.device.mxo/Contents/MacOS/mpr.device
+
+echo processing mpr.in.mxo
+install_name_tool -change $LIBLO_PATH @loader_path/../../../../support/liblo.7.dylib dist/Max/Mapper/externals/mpr.in.mxo/Contents/MacOS/mpr.in
+install_name_tool -change $LIBMAPPER_PATH @loader_path/../../../../support/libmapper.11.dylib dist/Max/Mapper/externals/mpr.in.mxo/Contents/MacOS/mpr.in
+
+echo processing mpr.out.mxo
+install_name_tool -change $LIBLO_PATH @loader_path/../../../../support/liblo.7.dylib dist/Max/Mapper/externals/mpr.out.mxo/Contents/MacOS/mpr.out
+install_name_tool -change $LIBMAPPER_PATH @loader_path/../../../../support/libmapper.11.dylib dist/Max/Mapper/externals/mpr.out.mxo/Contents/MacOS/mpr.out
+
+echo processing oscmulticast.mxo
+install_name_tool -change $LIBLO_PATH @loader_path/../../../../support/liblo.7.dylib dist/Max/Mapper/externals/oscmulticast.mxo/Contents/MacOS/oscmulticast
+install_name_tool -change $LIBMAPPER_PATH @loader_path/../../../../support/libmapper.11.dylib dist/Max/Mapper/externals/oscmulticast.mxo/Contents/MacOS/oscmulticast
+
+echo processing mapper.pd_darwin
+install_name_tool -change $LIBLO_PATH @loader_path/libs/liblo.7.dylib dist/pd/mapper/mapper.pd_darwin
+install_name_tool -change $LIBMAPPER_PATH @loader_path/libs/libmapper.11.dylib dist/pd/mapper/mapper.pd_darwin
+
 cd ..
 
 echo Done.
